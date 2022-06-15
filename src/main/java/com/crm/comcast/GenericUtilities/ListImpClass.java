adee0211e79716c43523567333f5d77997b09966
DELL <DELL@DESKTOP-D6BEUQT>
2022-06-16 00:43:54 +0530

    listener implementation class updated

--- src/main/java/com/crm/comcast/GenericUtilities/ListImpClass.java
+++ src/main/java/com/crm/comcast/GenericUtilities/ListImpClass.java
@@ -3,6 +3,7 @@ package com.crm.comcast.GenericUtilities;
 import java.io.File;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -12,9 +13,9 @@ public class ListImpClass implements ITestListener {
 	public void onTestFailure(ITestResult result) {
 Java_Utilities ju=new Java_Utilities();
 		String testname=result.getMethod().getMethodName();
-
-		EventFiringWebDriver edriver=new EventFiringWebDriver(Baseclass.sdriver);
-		File scr = edriver.getScreenshotAs(OutputType.FILE);
+TakesScreenshot tss=(TakesScreenshot)Baseclass.sdriver;
+		//EventFiringWebDriver edriver=new EventFiringWebDriver(Baseclass.sdriver);
+		File scr = tss.getScreenshotAs(OutputType.FILE);
 		File dst=new File("./screenshot/"+testname+"_"+ju.getData()+".png") ;
 		try {
 			FileUtils.copyFile(scr, dst);
